f208b54d2ff63789c1e183a4c077c8c81e88b45f
Dima Kozlovskyy <dima.kozlovskyy@gmail.com>
2026-04-12 21:22:35 +0000

    added assignments/assignment04-github.py

---
+++ assignments/assignment04-github.py
@@ -0,0 +1,29 @@
+# Importing pyGitHub API
+from github import Github, Auth
+from config import apikeys as cfg
+
+# Secure way to use API key from config.py
+# Couldnt get auth working so added AUTH.TOKEN
+auth = Auth.Token(cfg["githubkey"])
+g = Github(auth=auth)
+
+# Get the repository
+repo = g.get_user().get_repo('WSAA-coursework')
+
+# Get the file content
+file_content = repo.get_contents("assignments/file.txt")
+#print(file_content.decoded_content.decode("utf-8")) 
+
+# Getting bytes content and decoding it to string
+decoded_content = file_content.decoded_content.decode("utf-8")
+#print(decoded_content)
+
+# Replace "Andrew" with my name
+updated_content = decoded_content.replace("Andrew", "Dima")
+print(updated_content)
+
+# Using sha, as it is required by GitHub API to prevent conflicts
+# Update the file in the repository
+repo.update_file(file_content.path, "Updated file.txt", updated_content, file_content.sha)
+
+
